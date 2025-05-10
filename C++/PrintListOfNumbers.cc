c52bc0f1897e9e1e8bb9161390b83e26e3bf2063
natanzeraa <natanzeraaaaaa@gmail.com>
2025-05-10 11:08:54 -0300

    feat: implement vector iteration and print elements in C++

---
+++ C++/PrintListOfNumbers.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2,3};
+
+    for(int num : nums) {
+        cout << num << "\n";
+    }
+
+    return 0;
+}
